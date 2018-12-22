6cc4dfd7abc503846b9c70afcc55d02bbb32b90a
jajav33 <to.jajav33@gmail.com>
2018-12-22 15:00:41 -0800

    WIP - adding dragging slingshot ball.
    needs fine tuning but will fix it later.

---
+++ angrySanta/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public Rigidbody ball;
+	private bool isPressed = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPressed) {
+			Vector3 mouseToCamToBallPos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane - ball.position.z));
+			ball.position = (new Vector3(mouseToCamToBallPos.x, mouseToCamToBallPos.y, ball.position.z));
+		}
+	}
+
+	void OnMouseDown () {
+		isPressed = true;
+		ball.isKinematic = true;
+	}
+
+	void OnMouseUp () {
+		isPressed = false;
+		ball.isKinematic = false;
+	}
+}
