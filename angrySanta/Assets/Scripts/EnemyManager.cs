3c8482375267f0c7d1cf89f90b0f05094f0e93f0
jajav33 <to.jajav33@gmail.com>
2018-12-22 12:45:01 -0800

    WIP - ADDED bouncy Gift box.

--- angrySanta/Assets/Scripts/EnemyManager.cs
+++ angrySanta/Assets/Scripts/EnemyManager.cs
@@ -1,15 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
-	public enum AvailableEnemies {
-		test,
-		grandma
+	[System.Serializable]
+	public enum AvailableEnemiesEnum {
+		NONE,
+		TEST
 	}
+	public enum AvailableBuildings {
+		NONE,
+		TEST
+	}
+	public AvailableEnemiesEnum ENEMY_TO_GENERATE = 0;
+	public AvailableBuildings BUILDING_TO_GENERATE = 0;
+
+	public GameObject PREFAB_ENEMY_TEST;
+	public GameObject PREFAB_BUILDING_TEST;
+
 
 	// Use this for initialization
 	void Start () {
+		float x = 0f;
+		float y = 0f;
+
+		if (ENEMY_TO_GENERATE > 0) {
+			switch (ENEMY_TO_GENERATE) {
+			case AvailableEnemiesEnum.TEST:
+				Instantiate(PREFAB_ENEMY_TEST, new Vector3(x, y, 0), Quaternion.identity);
+				break;
+			case AvailableEnemiesEnum.NONE:
+			default:
+				break;
+			}
+		} else if (BUILDING_TO_GENERATE > 0) {
+			switch (BUILDING_TO_GENERATE) {
+			case AvailableBuildings.TEST:
+				break;
+			case AvailableBuildings.NONE:
+			default:
+				break;
+			}
+		}
+	}
+
+	void mySpawnEnemy () {
 		
 	}
 	
---
+++ angrySanta/Assets/Scripts/Projectile.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiile : MonoBehaviour {
+	public Rigidbody bulletPrefabs;
+	public GameObject cursor;
+	public LayerMask layer;
+
+
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void myLaunchProjectile () {
+//		// get mouse position according to camera;
+//		Ray camRay = cam.ScreenPointtoRay(Input.mousePosition);
+//		RaycastHit hit;
+//
+//		if (Physics.Raycast(camRay, out hit, 100f, layer) {
+//			cursor.SetActive(true);
+//			cursor.transform.position = hit.point + Vector3.up * 0.1f;
+//
+//			Vector3 Vo = myCalculateVelocity(hit.point, transform.position, 1f);
+//		}
+	}
+
+	Vector3 myCalculateVelocity (Vector3 target, Vector3 origin, float time) {
+		// define the dist x and y first;
+		Vector3 distanceY = target - origin;
+		Vector3 distanceX = distanceY;
+
+		// x travel will always have y as 0 since it is horizontal movement.
+		distanceX.y = 0f;
+
+		// create a float to hold magnitudes
+		float height = distanceY.y;
+		float deltaX = distanceX.magnitude;
+
+
+		float vx = deltaX / time;
+		float vy = height / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
+
+		Vector3 result = distanceX.normalized; // length of on.
+		result *= vx;
+		result.y = vy;
+
+		return result;
+ 	}
+}
