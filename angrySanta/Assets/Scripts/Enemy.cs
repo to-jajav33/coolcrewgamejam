4e3c4c31989308e981ab4825af1ba1e8f763f36c
jajav33 <to.jajav33@gmail.com>
2018-12-22 16:28:49 -0800

    Killing Enemies

---
+++ angrySanta/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float maxHealth = 4f;
+	private float currHealth;
+
+	void Start () {
+		this.currHealth = this.maxHealth;
+	}
+
+	void OnCollisionEnter (Collision colInfo) {
+		Debug.Log (colInfo.relativeVelocity.magnitude);
+		if (colInfo.relativeVelocity.magnitude > this.currHealth) {
+			this.myDieEnemy();
+		}
+	}
+
+	void myDieEnemy () {
+		Destroy (gameObject);
+	}
+
+}
